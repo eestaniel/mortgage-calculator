2b3723f906486ceacb125a0d5a39c134f1c6383d
EEstaniel <edwineestaniel93@gmail.com>
2024-07-01 00:31:29 -0700

    fix: result height

--- src/App.tsx
+++ src/App.tsx
@@ -26,69 +26,74 @@ const App: React.FC = () => {
 
     return (
         <main>
-            <form onSubmit={handleSubmission}>
-                <div className={styles.header_group}>
-                    <h1 className="preset-2">Mortgage Calculator</h1>
-                    <span className={`${styles.clear_all} preset-4`} onClick={clearAll}>Clear All</span>
-                </div>
+            <div className={styles.content}>
+                <form onSubmit={handleSubmission}>
+                    <div className={styles.header_group}>
+                        <h1 className="preset-2">Mortgage Calculator</h1>
+                        <span className={`${styles.clear_all} preset-4`} onClick={clearAll}>Clear All</span>
+                    </div>
 
-                <div className={styles.fields}>
-                    <NumberField
-                        id="mortgage-amount"
-                        label="Mortgage Amount"
-                        affix="£"
-                        isPrefix={true}
-                        func={setMortgageAmount}
-                        value={mortgageAmount}
-                        numberType="float"
-                        error={errors.mortgageAmount}
-                    />
-
-                    <div className={styles.term_rate_group}>
+                    <div className={styles.fields}>
                         <NumberField
-                            id="mortgage-term"
-                            label="Mortgage Term"
-                            affix="years"
-                            isPrefix={false}
-                            func={setMortgageTerm}
-                            value={mortgageTerm}
-                            numberType="int"
-                            maxValue={100}
-                            error={errors.mortgageTerm}
-                        />
-                        <NumberField
-                            id="interest-rate"
-                            label="Interest Rate"
-                            affix="%"
-                            isPrefix={false}
-                            func={setInterestRate}
-                            value={interestRate}
+                            id="mortgage-amount"
+                            label="Mortgage Amount"
+                            affix="£"
+                            isPrefix={true}
+                            func={setMortgageAmount}
+                            value={mortgageAmount}
                             numberType="float"
-                            maxValue={100}
-                            error={errors.interestRate}
+                            error={errors.mortgageAmount}
                         />
-                    </div>
 
-                    <div className={styles.radio_group}>
-                        <h2 className="preset-4">Mortgage Type</h2>
-                        <RadioField
-                            label="Repayment"
-                            checked={selectedOption === 'Repayment'}
-                            onChange={handleOptionChange}
-                        />
-                        <RadioField
-                            label="Interest Only"
-                            checked={selectedOption === 'Interest Only'}
-                            onChange={handleOptionChange}
-                        />
-                        {errors.selectedOption && selectedOption === '' && (
-                            <p className={styles.error_message}>{errors.selectedOption}</p>
-                        )}
+                        <div className={styles.term_rate_group}>
+                            <NumberField
+                                id="mortgage-term"
+                                label="Mortgage Term"
+                                affix="years"
+                                isPrefix={false}
+                                func={setMortgageTerm}
+                                value={mortgageTerm}
+                                numberType="int"
+                                maxValue={100}
+                                error={errors.mortgageTerm}
+                            />
+                            <NumberField
+                                id="interest-rate"
+                                label="Interest Rate"
+                                affix="%"
+                                isPrefix={false}
+                                func={setInterestRate}
+                                value={interestRate}
+                                numberType="float"
+                                maxValue={100}
+                                error={errors.interestRate}
+                            />
+                        </div>
+
+                        <div className={styles.radio_group}>
+                            <h2 className="preset-4">Mortgage Type</h2>
+                            <RadioField
+                                label="Repayment"
+                                checked={selectedOption === 'Repayment'}
+                                onChange={handleOptionChange}
+                            />
+                            <RadioField
+                                label="Interest Only"
+                                checked={selectedOption === 'Interest Only'}
+                                onChange={handleOptionChange}
+                            />
+                            {errors.selectedOption && selectedOption === '' && (
+                                <p className={styles.error_message}>{errors.selectedOption}</p>
+                            )}
+                        </div>
+                        <CustomButton
+                            onClick={(e) => handleSubmission(e as unknown as React.FormEvent<HTMLFormElement>)}/>
                     </div>
-                    <CustomButton onClick={(e) => handleSubmission(e as unknown as React.FormEvent<HTMLFormElement>)} />
+                </form>
+                <div className={styles.results}>
+                    {!results ? <EmptyResults/> : <ValidResults results={results} mortgageAmount={mortgageAmount}/>}
                 </div>
-            </form>
-            {!results ? <EmptyResults /> : <ValidResults results={results} mortgageAmount={mortgageAmount} />}
+            </div>
         </main>
     );
 };
