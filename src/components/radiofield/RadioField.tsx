d8b214e384b338115b04758b033f93b7d1a08a25
EEstaniel <edwineestaniel93@gmail.com>
2024-06-30 20:21:46 -0700

    feat: add background color on selection

--- src/components/radiofield/RadioField.tsx
+++ src/components/radiofield/RadioField.tsx
@@ -9,7 +9,7 @@ interface RadioFieldProps {
 
 const RadioField: React.FC<RadioFieldProps> = ({ label, onChange, checked }) => {
     return (
-        <label className={styles.radio_group} htmlFor={label}>
+        <label className={`${styles.radio_group} ${checked? styles.checked: ''}`} htmlFor={label}>
             <input type="radio" name={label} id={label} className={styles.radio_input} onChange={onChange} checked={checked}/>
             <span className={styles.radio_custom}></span>
             <span className={`${styles.radio_label} preset-3`}>{label}</span>
