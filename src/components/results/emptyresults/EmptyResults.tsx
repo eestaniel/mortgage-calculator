506251fe97572663e0f07d2a485dff3bdef1c47a
EEstaniel <edwineestaniel93@gmail.com>
2024-06-30 22:09:53 -0700

    feat: add ValidResults and emptyResults

---
+++ src/components/results/emptyresults/EmptyResults.tsx
@@ -0,0 +1,14 @@
+import styles from './EmptyResults.module.css';
+
+const EmptyResults = () => {
+    return (
+        <div className={styles.results}>
+            <img src="/public/images/illustration-empty.svg" alt="illustration-empty"/>
+            <h3 className={`preset-2`}>Results shown here</h3>
+            <p className={`preset-4`}>Complete the form and click “calculate repayments” to see what your monthly
+                repayments would be.</p>
+        </div>
+    );
+};
+
+export default EmptyResults;
---
+++ src/components/results/validresults/ValidResults.tsx
@@ -0,0 +1,47 @@
+import styles from './ValidResults.module.css';
+import React, {useEffect, useState} from "react";
+
+interface ValidResultsProps {
+    results: {
+        monthlyRepayment: number;
+        totalInterest: number;
+    },
+    mortgageAmount: string;
+}
+
+const ValidResults:React.FC<ValidResultsProps> = ({results, mortgageAmount}) => {
+    const [formattedMonthlyRepayment, setFormattedMonthlyRepayment] = useState('');
+    const [formattedTotalAmount, setFormattedTotalAmount] = useState('');
+
+    useEffect(() => {
+        // add comma to the monthly repayment value
+        setFormattedMonthlyRepayment(results.monthlyRepayment.toLocaleString());
+
+        // calulate the total amount to be repaid and format it
+        // remove the comma from the mortgage amount
+        const mortgageAmountNumber = parseFloat(mortgageAmount.replace(/,/g, ''));
+        const totalAmount = mortgageAmountNumber + results.totalInterest;
+        setFormattedTotalAmount(totalAmount.toLocaleString());
+    }, [results]);
+
+    return (
+        <div className={styles.content}>
+            <div className={`${styles.title_bar}`}>
+                <h3 className={`preset-2`}>Your results</h3>
+                <p className={`preset-4`}>Your results are shown below based on the information you provided. To adjust the results, edit the form and click “calculate repayments” again.</p>
+            </div>
+            <div className={styles.results}>
+                <div className={styles.monthly_group}>
+                    <h4>Your monthly repayments</h4>
+                    <p className={`preset-2`}>£ {formattedMonthlyRepayment}</p>
+                    <hr/>
+                    <h4>Total you'll repay over the term</h4>
+                    <p className={`preset-2`}>£ {formattedTotalAmount}</p>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default ValidResults;
